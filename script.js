5e1cc06a645735bd4e3c1987c0e1657d4d4e653b
Breki Ingibjargarson <breki@ccpgames.com>
2024-03-12 17:46:45 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 function printHello() {
     console.log("Hello, world!");
 }
+
+// Asks for user permission to access the camera and displays the video stream.
+function startVideo() {
+    const video = document.getElementById('videoElement');
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(function(stream) {
+                video.srcObject = stream;
+            })
+            .catch(function(err) {
+                console.log("Something went wrong!", err);
+            });
+    }
+}
+
+window.onload = startVideo;
